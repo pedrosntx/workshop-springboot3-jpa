76ff7f926748e7a25a79fab01b62168738980a0e
Pedro Santos <phgomes03122004@gmail.com>
2025-07-29 20:21:06 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/pedrosantos/course/entities/User.java
+++ src/main/java/com/pedrosantos/course/entities/User.java
@@ -1,8 +1,11 @@
 package com.pedrosantos.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +20,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
@@ -70,6 +77,10 @@ public class User implements Serializable {
         this.phone = phone;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -81,6 +92,4 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
-
-
 }
