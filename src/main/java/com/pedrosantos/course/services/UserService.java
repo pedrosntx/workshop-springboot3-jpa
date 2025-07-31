34e354573bbd6338fd855a35f47d29afd0b385e2
Pedro Santos <phgomes03122004@gmail.com>
2025-07-31 19:36:23 -0300

    Exception handling - delete

--- src/main/java/com/pedrosantos/course/services/UserService.java
+++ src/main/java/com/pedrosantos/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.pedrosantos.course.services;
 
 import com.pedrosantos.course.entities.User;
 import com.pedrosantos.course.repositories.UserRepository;
+import com.pedrosantos.course.services.exceptions.DatabaseException;
 import com.pedrosantos.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
